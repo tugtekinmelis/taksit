544dfbf9dfb9f2617736d9fdaa9e9102e4f3e0a2
tugtekinmelis <145451246+tugtekinmelis@users.noreply.github.com>
2023-10-02 16:37:40 +0300

    Add files via upload

---
+++ ürün taksit hesaplama/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace ürün_taksit_hesaplama
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbTaksit = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTutar = new System.Windows.Forms.TextBox();
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.lblSonuc = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.tbTaksit)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbTaksit
+            // 
+            this.tbTaksit.Location = new System.Drawing.Point(67, 64);
+            this.tbTaksit.Maximum = 12;
+            this.tbTaksit.Minimum = 1;
+            this.tbTaksit.Name = "tbTaksit";
+            this.tbTaksit.Size = new System.Drawing.Size(330, 45);
+            this.tbTaksit.TabIndex = 0;
+            this.tbTaksit.Value = 1;
+            this.tbTaksit.Scroll += new System.EventHandler(this.tbTaksit_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(441, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(15, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "0";
+            // 
+            // txtTutar
+            // 
+            this.txtTutar.Location = new System.Drawing.Point(67, 125);
+            this.txtTutar.Name = "txtTutar";
+            this.txtTutar.Size = new System.Drawing.Size(100, 20);
+            this.txtTutar.TabIndex = 2;
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.Location = new System.Drawing.Point(243, 121);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(121, 27);
+            this.btnHesapla.TabIndex = 3;
+            this.btnHesapla.Text = "Hesapla";
+            this.btnHesapla.UseVisualStyleBackColor = true;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // lblSonuc
+            // 
+            this.lblSonuc.AutoSize = true;
+            this.lblSonuc.Location = new System.Drawing.Point(64, 188);
+            this.lblSonuc.Name = "lblSonuc";
+            this.lblSonuc.Size = new System.Drawing.Size(35, 13);
+            this.lblSonuc.TabIndex = 4;
+            this.lblSonuc.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblSonuc);
+            this.Controls.Add(this.btnHesapla);
+            this.Controls.Add(this.txtTutar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbTaksit);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.tbTaksit)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar tbTaksit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTutar;
+        private System.Windows.Forms.Button btnHesapla;
+        private System.Windows.Forms.Label lblSonuc;
+    }
+}
+
---
+++ ürün taksit hesaplama/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ürün_taksit_hesaplama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHesapla_Click(object sender, EventArgs e)
+        {
+            int taksit, tutar;
+            double taksitliTutar; 
+            taksit = tbTaksit.Value;
+            tutar = Convert.ToInt32(txtTutar.Text);
+
+            taksitliTutar = (double)tutar / taksit;
+
+            lblSonuc.Text = taksitliTutar.ToString();
+
+        }
+
+        private void tbTaksit_Scroll(object sender, EventArgs e)
+        {
+            lblSonuc.Text = tbTaksit.Value.ToString();
+        }
+    }
+}
